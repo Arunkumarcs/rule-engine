ef4977df2086495557e14ff2c77943fbd9e06e36
arunt2s <arunkumar.h@touch2success.com>
2025-04-11 05:42:15 +0530

    presetup

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
+  collectCoverage: true,
   testEnvironment: "node",
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
   },
   testMatch: ["**/*.test.ts", "**/*.spec.ts"],
+  coverageReporters: ["lcov", "json-summary"],
 };
