9a8b4fa6ffa795e79b7f0d58b4cef21b606bfb2f
Arunkumarcs <arunkumarmessenger@gmail.com>
2025-04-13 22:05:47 +0530

    fix: commitlint (#30)

--- release.config.js
+++ release.config.js
@@ -1,5 +1,23 @@
 module.exports = {
-  branches: ["main"],
+  branches: [
+    "main",
+    {
+      name: "beta",
+      prerelease: true, // auto suffixes like `-beta.1`
+    },
+    {
+      name: "alpha",
+      prerelease: true, // specific pre-release label
+    },
+    {
+      name: "dev",
+      prerelease: true, // specific pre-release label
+    },
+    {
+      name: "rc",
+      prerelease: true, // specific pre-release label
+    },
+  ],
   range: ">=1.4.3",
   plugins: [
     "@semantic-release/commit-analyzer",
